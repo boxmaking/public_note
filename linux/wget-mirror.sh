e9f072c6eab123cb8d844790f5d3305b8d5765c7
pengshao <pengshao58@gmail.com>
2020-04-21 23:32:57 +0800

    2020-04-21

--- linux/wget-mirror.sh
+++ linux/wget-mirror.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-wget -c --mirror -p --convert-links -P ./myblog 
+#模板文件
+wget -c --mirror -p --convert-links -P ./****** URL
+
+
+
+#王垠的博客
+#wget -c --mirror -p --convert-links -P ./myblog www.ctex.org/documents/shredder/
