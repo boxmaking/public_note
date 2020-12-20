dbfbdd0a88da7377d07cfe36a744bbd93f000c96
pengshao <pengshao@gmail.com>
2020-12-20 20:07:12 +0800

    12202020

--- shellscript/youtube.sh
+++ shellscript/youtube.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp ~/shellscript/youtube.sh ./
+
 #URL=""
 #URL=""
 #URL=""
@@ -24,82 +26,77 @@
 #c语言教程-temp #playlist="https://www.youtube.com/playlist?list=PLY_qIufNHc293YnIjVeEwNDuqGo8y2Emx"
 #artistic-son-nkife-temp#playlist="https://www.youtube.com/playlist?list=PL3uNOeI85OO5WgViMrcV1gYU8eMie4bYQ"
 
-#salvik-tely-csgo-knife
-playlist="https://www.youtube.com/playlist?list=PLoMYuVm4uducpAs5oMt5RIDdAe40BbAOp"
+#salvik-tely-csgo-knife #playlist="https://www.youtube.com/playlist?list=PLoMYuVm4uducpAs5oMt5RIDdAe40BbAOp"
 
-#salvik-custom-knife
-#playlist="https://www.youtube.com/playlist?list=PLoMYuVm4udufkeHggidNf_efTQI-CipXR"
+#salvik-custom-knife #playlist="https://www.youtube.com/playlist?list=PLoMYuVm4udufkeHggidNf_efTQI-CipXR"
 
-#salivik-JAPANESE KNIVES
-#playlist="https://www.youtube.com/playlist?list=PLoMYuVm4uducJ4pOQsv4v1B2SU2kJMCXm"
+#salivik-JAPANESE KNIVES #playlist="https://www.youtube.com/playlist?list=PLoMYuVm4uducJ4pOQsv4v1B2SU2kJMCXm"
 
-#awe me-recent upload
-#playlist="https://www.youtube.com/playlist?list=PLUUGFk1wE5OFjtAkK-YT8-xIinPl-J0Wb"
+#awe me-recent upload #playlist="https://www.youtube.com/playlist?list=PLUUGFk1wE5OFjtAkK-YT8-xIinPl-J0Wb"
 
-#avmake-knife
-#playlist="https://www.youtube.com/playlist?list=PLwnDryJ3YYJT7VXLaCchu6ZjG1H9aVK45"
-#avmake-fun-pro
-#playlist="https://www.youtube.com/playlist?list=PLwnDryJ3YYJT7qePRGB9IT30Y49yblvcH"
+#avmake-knife #playlist="https://www.youtube.com/playlist?list=PLwnDryJ3YYJT7VXLaCchu6ZjG1H9aVK45"
+#avmake-fun-pro #playlist="https://www.youtube.com/playlist?list=PLwnDryJ3YYJT7qePRGB9IT30Y49yblvcH"
 
-#make-it-extreme
-#playlist="https://www.youtube.com/playlist?list=PLgXx5D-u4k4iLwym_J40EeAIbTkxJfbA9"
-##make-it-extreme-how-to-make
-#playlist="https://www.youtube.com/playlist?list=PLgXx5D-u4k4jxzB2SdIgxwXjjY9vIWgko&pbjreload=101"
+#make-it-extreme #playlist="https://www.youtube.com/playlist?list=PLgXx5D-u4k4iLwym_J40EeAIbTkxJfbA9"
+##make-it-extreme-how-to-make #playlist="https://www.youtube.com/playlist?list=PLgXx5D-u4k4jxzB2SdIgxwXjjY9vIWgko&pbjreload=101"
 
-#make in poland all
-#playlist="https://www.youtube.com/playlist?list=PLtp0qX2WkOnVCzOicISW2uOdyyLEcfKdr"
-
-#The other Finnish guy-diy-tools
-#playlist="https://www.youtube.com/playlist?list=PLALEzF7YE8jK7LDOeF0TwFj02DCsR6361"
+##my mechanics全部的,翻新系列 #playlist="https://www.youtube.com/playlist?list=PLN0SuqPcbLqFVPEzpvzcXAQO0e2zcdoDU"
 
 
-#Scott Turner-diy-all
-#playlist="https://www.youtube.com/playlist?list=PLeZ7YqfvgMc0VCJjtKixWgSO9I0qc0cCj"
+#make in poland all #playlist="https://www.youtube.com/playlist?list=PLtp0qX2WkOnVCzOicISW2uOdyyLEcfKdr"
+
+#The other Finnish guy-diy-tools #playlist="https://www.youtube.com/playlist?list=PLALEzF7YE8jK7LDOeF0TwFj02DCsR6361"
 
 
-#TheMetalRaymond diy tool
-#playlist="https://www.youtube.com/playlist?list=PLdyZkLBbY2DbrBjKkdaB8pKRirtphcIwB"
+#Scott Turner-diy-all #playlist="https://www.youtube.com/playlist?list=PLeZ7YqfvgMc0VCJjtKixWgSO9I0qc0cCj"
 
 
-#Torbjörn Åhman
-#playlist="https://www.youtube.com/playlist?list=PLxTbEbyiv6g4TqGERC1oQ4X6rxp9ioCOQ"
+#TheMetalRaymond diy tool #playlist="https://www.youtube.com/playlist?list=PLdyZkLBbY2DbrBjKkdaB8pKRirtphcIwB"
 
-#伝統工芸 青山スクエア 
-#playlist="https://www.youtube.com/playlist?list=PLrV_BP5Fb2nHjABJ9jF06LhpR9VSCmKGb"
 
-#基地base
-#playlist="https://www.youtube.com/playlist?list=PLOd7JttKoK9gTQsrUGUIkrmoTCw9gaoi5"
+#Torbjörn Åhman #playlist="https://www.youtube.com/playlist?list=PLxTbEbyiv6g4TqGERC1oQ4X6rxp9ioCOQ"
 
-#青蛙品魔戒
-#playlist="https://www.youtube.com/playlist?list=PLg45KLfneaCQXaCXZz9aOvrgEhisYWzUB"
-#星战
-#playlist="https://www.youtube.com/playlist?list=PLg45KLfneaCQVWqoPhxtSUBYNPHSgTKQY"
-#哈比人
-#playlist="https://www.youtube.com/playlist?list=PLg45KLfneaCS_jbeV2Hn0Tqrp6s9quSMh"
-#哈利波特-temp
-#playlist="https://www.youtube.com/playlist?list=PLg45KLfneaCTXxacXI9WEZ_26lid_D6YU"
+#伝統工芸 青山スクエア  #playlist="https://www.youtube.com/playlist?list=PLrV_BP5Fb2nHjABJ9jF06LhpR9VSCmKGb"
 
-#youtube赚钱
-#playlist="https://www.youtube.com/playlist?list=PLwwPq48LW7z9mDJUYi-guT9hcVVwls1UX"
-#google赚钱
-#playlist="https://www.youtube.com/playlist?list=PLwwPq48LW7z9VPcRWJDr1NiOGo8lub8il"
-#ig赚钱
-#playlist="https://www.youtube.com/playlist?list=PLwwPq48LW7z_yhXNSvejohJahgsMuhO9G"
+#基地base #playlist="https://www.youtube.com/playlist?list=PLOd7JttKoK9gTQsrUGUIkrmoTCw9gaoi5"
 
-#pre教程
-#playlist="https://www.youtube.com/playlist?list=PLwwPq48LW7z-SmRdfCS-0A1cq3KF7-RSx&pbjreload=101"
-#职人工厂-update-temp
-#playlist="https://www.youtube.com/watch?v=m3l0jOMQ1cw&list=PLBQxWBmydvh-bKEHVcK4ChhsWB7Mtd6Zv"
+#青蛙品魔戒 #playlist="https://www.youtube.com/playlist?list=PLg45KLfneaCQXaCXZz9aOvrgEhisYWzUB"
+#哈利波特-temp #playlist="https://www.youtube.com/playlist?list=PLg45KLfneaCTXxacXI9WEZ_26lid_D6YU"
+
+#koss-knife-making #playlist="https://www.youtube.com/playlist?list=PLbZO7Ywxbu1J4RJ_gCiuczhSvHxph-OF1"
+#koss-diytool #playlist="https://www.youtube.com/playlist?list=PLbZO7Ywxbu1L59srbT2mWRzgepEywTC4W"
+#koss-odd-job #playlist="https://www.youtube.com/playlist?list=PLbZO7Ywxbu1K2gYtodyd_InHD5IjyWpd9"
+#koss-Restorations #playlist="https://www.youtube.com/playlist?list=PLbZO7Ywxbu1Lp3NNYDD_JzPJ18zRHvQAe"
+
+#Pask-Makes-Make-Videos-Not-Tools #playlist="https://www.youtube.com/playlist?list=PLuKoJWkcJCp4mQ9w2-jobpJpYT-oimJAD"
+#Pask-Makes-homemade-tool #playlist="https://www.youtube.com/playlist?list=PLuKoJWkcJCp5xXP6ijx4g3lUxwcob9Lv5"
+#Pask-Makes-knife-making #playlist="https://www.youtube.com/watch?v=b8zOQ4hyQzc&list=PLuKoJWkcJCp7NUnu_9cRLNLERk58bVO5r"
+
+
+#pre教程 #playlist="https://www.youtube.com/playlist?list=PLwwPq48LW7z-SmRdfCS-0A1cq3KF7-RSx&pbjreload=101"
+
+#职人工厂-update-temp #playlist="https://www.youtube.com/watch?v=m3l0jOMQ1cw&list=PLBQxWBmydvh-bKEHVcK4ChhsWB7Mtd6Zv"
+
 #日本巡礼
 #playlist="https://www.youtube.com/playlist?list=PLBQxWBmydvh_GdpHOMwJpqyQ-J1qlRFeA"
 
+#袁游日本行
+#playlist="https://www.youtube.com/playlist?list=PLnzvH6pAJKSoDie5UULhHM88ThSQLb6Uf"
 
-#playlist=""
-#playlist=""
-#playlist=""
+#兰爸爸说故事-日本
+#playlist="https://www.youtube.com/playlist?list=PLx6AWQKwKPqA8mZOD8tq_vMqjkeMEEhM4"
+
+#秋田akita-真田丸诉说
+#playlist="https://www.youtube.com/playlist?list=PLx61-GEfMQhprivJadEtwwhQPIgbjFE11"
+
+#秋田akita-日本人物
+#playlist="https://www.youtube.com/playlist?list=PLx61-GEfMQhq78K4OpFyNh0a4RrioKcB0"
+
+#围城  陈道明版
+#playlist="https://www.youtube.com/playlist?list=PLQ-7zWAY9S0X72vwyRdtyBsVsk5Abs0xD"
+
+#解密档案,香港黑帮往事
+playlist="https://www.youtube.com/playlist?list=PLW30Gw1qGzqijsTtehGBOs06u3TQfoA5d"
 
 
 youtube-dl -citf best  ${playlist} 
-
-
-
